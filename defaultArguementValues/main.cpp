28060c652003886d9dc5648a1f12d01438acdb32
Paul McQuaid <pmcquaid32@gmail.com>
2022-04-06 21:59:35 +0100

    Added to DefaultArgs, Created Overloading Function Folder

--- defaultArguementValues/main.cpp
+++ defaultArguementValues/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
 double calc_cost(double base_cost, double tax_rate = 0.06,
                  double shipping = 3.50);
 
+
+void greeting(string name, string prefix = "Mr.", string suffix = "");
+
 int main()
 {
+    greeting("Paul McQuaid", "Mr.", "B.A");
+    greeting("Jo Greene", "Mrs");
+    greeting("Sean Hughs", "Dr.", "Ph.D.");
+    
+
     double cost{0};
     cost = calc_cost(100.0,0.08,4.25);
 
@@ -27,4 +36,9 @@ double calc_cost(double base_cost, double tax_rate, double shipping){
 
     return base_cost += (base_cost*tax_rate)+shipping;
 
+}
+
+void greeting(string name, string prefix, string suffix){
+
+    cout << "Hello " + prefix + " " + name + " " + suffix << endl;
 }
